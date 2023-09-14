7dfc8cca205e1c8c5fa88c079f3f944cca4c41e5
den-duar <2021211510079@iesp.edu.br>
2023-09-14 20:46:10 -0300

    fix: estrutura desejada

---
+++ ProjetoReactNative/App.js
@@ -0,0 +1,12 @@
+
+import App from "./src/App";
+
+export default function App() {
+  return (
+
+      <App/>
+
+  );
+}
+
+
---
+++ ProjetoReactNative/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Primeiro from './components/Primeiro';
+
+const App = () =>  {
+  return (
+
+      <Primeiro/>
+
+  )
+}
+
+export default App;
+
+
