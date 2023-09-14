63a7a0d07b506a5a2a0a3cc1e8efed153aba3f16
den-duar <2021211510079@iesp.edu.br>
2023-09-14 20:36:45 -0300

    feature: criacao projeto e primeiro componente

---
+++ ProjetoReactNative/src/components/Primeiro.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Primeiro = () => {
+  return (
+
+        <View>
+            <Text>Ola mundo!</Text>
+        </View>
+
+  )
+}
+
+export default Primeiro
