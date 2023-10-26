6c5f5e34c8d48d2a67e935eaf29fa86cd76a8029
den-duar <2021211510079@iesp.edu.br>
2023-10-26 20:39:18 -0300

    adição componentes

--- ProjetoReactNative/App.jsx
+++ ProjetoReactNative/App.jsx
@@ -1,17 +1,25 @@
 import Primeiro from "./src/components/Primeiro";
 import PrimeiroProps from "./src/components/PrimeiroProps";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import Estilos from "./src/components/Estilos";
 import { Group1, Group2 } from "./src/components/Multi";
+import Aleatorio from "./src/components/Aleatorio";
+import VerificaNum from "./src/components/VerificaNum";
 
 export default function App() {
   return (
 
       <View style = {Estilos.view}>
+        <Aleatorio min={7} max = {99}/>
+        <Aleatorio min={7} max = {99}/>
+        <Aleatorio min={7} max = {99}/>
         <Primeiro/>
         <PrimeiroProps/>
         <Group1/>
         <Group2/>
+        <VerificaNum valor1={10} valor2={9}/>
+        <VerificaNum valor1={1} valor2={90}/>
+        <VerificaNum valor1={2} valor2={2}/>
       </View>
 
   );
