7d00990e58d73dcdda895e031d9b2dbb6e5687f2
den-duar <2021211510079@iesp.edu.br>
2023-11-16 20:34:29 -0300

    Adicionado secureText para password

--- ProjetoReactNative/src/components/Formulario.jsx
+++ ProjetoReactNative/src/components/Formulario.jsx
@@ -24,6 +24,7 @@ export default props =>{
 
             <TextInput
                 style={{borderWidth:1}}
+                secureTextEntry
                 keyboardType="number-pad"
                 placeholder="Password"
                 value={password}
